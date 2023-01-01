f66f0a127e17665e656d2de019dba572d9441663
WangDanmeng <WangDanmeng98@163.com>
2023-01-01 14:07:31 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Diamond />
+</template>
+
+<script>
+import Diamond from './components/Diamond.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Diamond,
+  },
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue' // 创建app实例
+import App from './App.vue' // 组件
+import './index.css'
+
+createApp(App).mount('#app') // 接收组件,挂载到div#app上面
