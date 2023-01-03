016c24a9bc26a7320a8f087569f28d05bad08411
WangDanmeng <WangDanmeng98@163.com>
2023-01-03 14:25:12 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>Dialog Demo</div>
+  <h1>实例1</h1>
+  <Dialog />
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: { Dialog },
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>ok</Button>
+      <Button>cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+  components: { Button },
+}
+</script>
