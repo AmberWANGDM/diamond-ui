5cf85d9b3bbd56aff7b849fa67b138c268d30add
WangDanmeng <WangDanmeng98@163.com>
2023-01-03 10:18:17 +0800

    button支持theme

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -46,5 +46,23 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.dm-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.dm-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 </style>
