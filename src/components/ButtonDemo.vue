03d242a2c40d8351a0d80b20e0f9972e5cbf7914
WangDanmeng <WangDanmeng98@163.com>
2023-01-03 09:40:49 +0800

    button添加theme

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,9 +2,10 @@
   <div>Button Demo</div>
   <h1>示例1</h1>
   <div>
-    <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="big"
-      >你好</Button
-    >
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,50 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
-      <slot />
-    </button>
-  </div>
+  <button class="dm-btn" :class="`theme-${theme}`">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
 export default {
-  inheritAttrs: false,
-  setup(props, context) {
-    // 剩余操作符(数组)
-    const { size, ...rest } = context.attrs
-    return { size, rest }
-  },
+  props: { theme: { type: String, default: 'button' } },
 }
 </script>
 
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.dm-btn {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 2px 0 rgb(0 0 0 / 2%);
+  // + 组合器选择相邻元素，即后一个元素紧跟在前一个之后，并且共享同一个父节点。 语法：A + B 例子：h2 + p 会匹配所有紧邻在 <h2> (en-US) 元素后的 <p> 元素。
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  // 兼容firefox
+  &::-moz-focus-inner {
+    border: 0;
+  }
 }
 </style>
