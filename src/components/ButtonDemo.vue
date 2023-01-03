04bf9ae4aa69e130d76b6870ea2bb7a980cbd328
WangDanmeng <WangDanmeng98@163.com>
2023-01-03 09:15:20 +0800

    属性绑定细节

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,13 +2,21 @@
   <div>Button Demo</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="big"
+      >你好</Button
+    >
   </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: Button,
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('hi')
+    }
+    return { onClick }
+  },
 }
 </script>
