ee9f93d33072f7c723cb41172fa5ba719c8db2a0
WangDanmeng <WangDanmeng98@163.com>
2023-01-03 14:48:24 +0800

    Dialog css1

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>ok</Button>
-      <Button>cancel</Button>
-    </footer>
+  <div class="dm-dialog-overlay"></div>
+  <div class="dm-dialog-wrapper">
+    <div class="dm-dialog">
+      <header>标题<span class="dm-dialog-close"></span></header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button level="main">确定</Button>
+        <Button>取消</Button>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -18,3 +21,76 @@ export default {
   components: { Button },
 }
 </script>
+<style lang="scss">
+$radius: 4px;
+$black: #1e2128;
+$border-color: #e5e6eb;
+$hover-color: #f2f3f5;
+.dm-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  > header {
+    padding: 12px 12px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  > main {
+    padding: 12px 16px;
+  }
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 22px;
+    height: 22px;
+    border-radius: 50%;
+    cursor: pointer;
+    transition: all 0.25s ease-in-out;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: $black;
+      width: 50%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+    &:hover {
+      background-color: $hover-color;
+    }
+  }
+}
+</style>
