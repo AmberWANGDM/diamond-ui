6d207157f89398da5b189371eadb3b4627c59050
WangDanmeng <WangDanmeng98@163.com>
2023-01-04 09:50:18 +0800

    修改 lib.scss 引入顺序

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue' // 创建app实例
+import './lib/dm.scss'
 import App from './App.vue'
 import './index.scss'
 import { router } from './router'
-import './lib/dm.scss'
 
 const app = createApp(App)
 app.use(router)
