31215c8083dbf587b01377c55e5387e8ff38588d
WangDanmeng <WangDanmeng98@163.com>
2023-01-04 09:16:02 +0800

    获取子组件的类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Tabs</div>
+  <div>
+    Tabs组件
+    <component :is="defaults[0]"></component>
+    <component :is="defaults[1]"></component>
+  </div>
 </template>
+
+<script lang="ts">
+import { def } from '@vue/shared'
+import { takeCoverage } from 'v8'
+import Tab from './Tab.vue'
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs子标签必须为Tab')
+      }
+    })
+    return { defaults }
+  },
+}
+</script>
