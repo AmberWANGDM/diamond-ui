aaba6a8c14df95beae6d6cb98dd37fb3bf7cae13
WangDanmeng <WangDanmeng98@163.com>
2023-02-08 12:29:40 +0800

    feat: 重写函数调用对话框展示

--- src/components/Dialog/Dialog2.demo.vue
+++ src/components/Dialog/Dialog2.demo.vue
@@ -3,29 +3,23 @@
   <Button @click="showDialog">toggle</Button>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { h } from 'vue'
 import Button from '../../lib/Button.vue'
 import { openDialog } from '../../lib/showDialog'
-
-export default {
-  setup() {
-    // 一句代码显示Dialog
-    const showDialog = () => {
-      openDialog({
-        title: h('strong', {}, '标题'),
-        content: 'hello world',
-        closeOnClickOverlay: false,
-        ok() {
-          console.log('ok')
-        },
-        cancel() {
-          console.log('cancel')
-        },
-      })
-    }
-    return { showDialog }
-  },
-  components: { Button },
+// 一句代码显示Dialog
+const showDialog = () => {
+  openDialog({
+    title: h('strong', {}, '标题'),
+    content: 'hello world',
+    closeOnClickOverlay: false,
+    ok() {
+      console.log('ok')
+    },
+    cancel() {
+      console.log('cancel')
+    },
+  })
 }
+
 </script>
--- src/lib/showDialog.ts
+++ src/lib/showDialog.ts
@@ -1,33 +1,35 @@
-import { createApp,h } from 'vue'
+import { h, render } from 'vue'
 import Dialog from './Dialog.vue'
-export const openDialog = (options)=>{
-  const {title,content,ok,cancel,closeOnClickOverlay} = options
+// 通常使用 声明式渲染 <Dialog :visible="bool" @update:visible="bool=$event" />
+// 以下是使用函数调用组件，命令式写法
+export const openDialog = options => {
+  const { title, content, ok, cancel, closeOnClickOverlay } = options
+  // 1. 调用h把组件变成虚拟DOM
+  const VNode = h(
+    Dialog,
+    // 传给组件的props
+    {
+      // v-model需要扩展为modelValue和onUpdate:modelValue
+      visible: true,
+      'onUpdate:visible': newValue => {
+        if (!newValue) {
+          render(null, div)
+          div.remove()
+        }
+      },
+      ok,
+      cancel,
+      closeOnClickOverlay,
+    },
+    // slot https://cn.vuejs.org/guide/extras/render-function.html#passing-slots
+    {
+      title: () => title,
+      content: () => content,
+    }
+  )
+  // 2. 准备好挂载节点
   const div = document.createElement('div')
   document.body.appendChild(div)
-  const app = createApp({
-    render(){
-      // https://vuejs.org/api/render-function.html#h
-      return h(
-        Dialog,
-        // props
-        {
-        visible:true,
-        'onUpdate:visible':(newValue)=>{
-          if(newValue===false){
-            app.unmount()
-            div.remove()
-          }
-        },
-        ok,
-        cancel,
-        closeOnClickOverlay
-      },
-      // slots
-      {
-        title,
-        content
-      })
-    }
-  })
-  app.mount(div)
+  // 3. render渲染虚拟dom到真实dom
+  render(VNode, div)
 }
